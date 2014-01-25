542ad7306983720edcd42e14cb735b0e5b188baa
Drummond Ogilvie <spare@daogilvie.com>
2014-01-25 22:54:56 +0000

    Consolidating, removing sound effects.

--- invaders.py
+++ invaders.py
@@ -1,8 +1,7 @@
 """Invaders From Space! A Totally Original Game For Digimakers"""
 
 import pyglet
-from player import Player
-from alien import Alien
+from objects import Player, Alien
 
 
 class InvadersWindow(pyglet.window.Window):
@@ -14,14 +13,22 @@ class InvadersWindow(pyglet.window.Window):
         # Create pyglet window
         super(InvadersWindow, self).__init__()
 
+        # Game over label, also used for checking the state
+        self.game_over_label = None
+
         #  Add the alien tracker
         self.aliens = []
         self.spawn_alien_row(number_of_aliens=4)
 
+        self.lasers = []
+
+        # Make it a bit harder by dumping a load of invaders
+        self.lurch_aliens_forward()
+        self.lurch_aliens_forward()
+
         # Add the timed functions to make them jump about
-        pyglet.clock.schedule_interval(self.lurch_aliens_forward, 5)
         pyglet.clock.schedule_interval(self.change_alien_direction, 5)
-        pyglet.clock.schedule_once(self.spawn_alien_row, 5, (4))
+        pyglet.clock.schedule_interval(self.lurch_aliens_forward, 5)
 
         # Add the player and bullet tracker
         self.player = Player(window=self)
@@ -41,35 +48,72 @@ class InvadersWindow(pyglet.window.Window):
         for alien in self.aliens:
             alien.draw()
 
+        for laser in self.lasers:
+            laser.draw()
+
+        if self.game_over_label is not None:
+            self.game_over_label.draw()
+
     def update(self, delta_time):
         """Perform frame-rate indepent updates of game objects"""
-        have_collided = InvadersWindow.have_collided
-        self.player.update(delta_time=delta_time)
 
-        # Update all the bullets
-        for bullet in self.bullets:
-            bullet.update(delta_time=delta_time)
-            # Check collisions
+        if self.game_over_label is None:
+            have_collided = InvadersWindow.have_collided
+            self.player.update(delta_time=delta_time)
+
+            # Update all the bullets
+            for bullet in self.bullets:
+                bullet.update(delta_time=delta_time)
+                # Check collisions
+                for alien in self.aliens:
+                    if have_collided(bullet, alien):
+                        bullet.destroyed = True
+                        alien.explode()
+
+            # Update all the lasers
+            for laser in self.lasers:
+                laser.update(delta_time=delta_time)
+                # Check collision
+                if InvadersWindow.have_collided(laser, self.player):
+                    self.player.explode()
+                    self.game_over(you_won=False)
+
+            # Remove bullets that have gone off the screen
+            self.bullets = [
+                b for b in self.bullets if b.sprite.y < self.height
+                and not b.destroyed]
+
+            # Remove the aliens that are destroyed
+            self.aliens = [a for a in self.aliens if not a.destroyed]
+
+            # Remove lasers that have gone off the screen
+            self.lasers = [
+                b for b in self.lasers if b.sprite.y > 0
+                and not b.destroyed]
+
+            # Make the aliens fire!
             for alien in self.aliens:
-                if have_collided(bullet, alien):
-                    bullet.destroyed = True
-                    alien.explode()
+                alien.fire()
 
-        # Remove bullets that have gone off the screen
-        self.bullets = [
-            b for b in self.bullets if b.sprite.y < self.height
-            and not b.destroyed]
-        self.aliens = [a for a in self.aliens if not a.destroyed]
+            # Do the end game victory check
+            if len(self.aliens) == 0:
+                self.game_over(you_won=True)
 
     def change_alien_direction(self, delta_time):
         """Make aliens strafe in a different direction"""
         for alien in self.aliens:
             alien.head_right = not alien.head_right
 
-    def lurch_aliens_forward(self, delta_time):
+    def lurch_aliens_forward(self, delta_time=None):
         """Make aliens lurch forward"""
-        for alien in self.aliens:
-            alien.lurch()
+        if self.game_over_label is None:
+            for alien in self.aliens:
+                if not alien.lurch():
+                    # lurch() returns false if the alien
+                    # has reached you
+                    self.game_over(you_won=False)
+                    return
+            self.spawn_alien_row(number_of_aliens=4)
 
     def spawn_alien_row(self, delta_time=None, number_of_aliens=4):
         """Make a row of aliens at the top of the screen"""
@@ -80,20 +124,42 @@ class InvadersWindow(pyglet.window.Window):
 
     @staticmethod
     def have_collided(bullet, alien):
-        # Ignore aliens that have exploded
-        if alien.exploded:
-            return False
+        """Check collisions between aliens and bullets"""
+
+        if isinstance(alien, Player):
+            # The player's collisions should worry about
+            # the bottom of the bullet
+            bullet_tip = bullet.sprite.y
+        else:
+            bullet_tip = bullet.sprite.y + bullet.sprite.height
+            # Ignore aliens that have exploded
+            if alien.exploded:
+                return False
 
         # Check that the tip of the bullet is within the alien sprite
         if bullet.sprite.x > alien.sprite.x \
                 and bullet.sprite.x < alien.sprite.x + alien.sprite.width:
-            bullet_tip = bullet.sprite.y + bullet.sprite.height
+
             if bullet_tip > alien.sprite.y \
                     and bullet_tip < alien.sprite.y + alien.sprite.height:
                 return True
 
         # By default say no.
         return False
+
+    def game_over(self, you_won=False):
+        """Game over!"""
+        if you_won:
+            text = "You Win!"
+        else:
+            text = "Game Over"
+
+        self.game_over_label = pyglet.text.Label(
+            text,
+            font_size=30,
+            anchor_x="center",
+            x=self.width/2,
+            y=self.height/2)
 
 
 def run_game():
---
+++ objects.py
@@ -0,0 +1,167 @@
+"""Contains code for all the game objects."""
+
+import pyglet
+from random import random
+
+
+class Bullet(object):
+    """Deal with collisions, and bullets pathing and stuff."""
+    image = pyglet.resource.image("images/bullet.png")
+    image.anchor_x = image.width / 2
+    speed = 180
+    scale = 0.2
+
+    def __init__(self, x_pos):
+        """Initialise a newly created bullet"""
+        self.sprite = pyglet.sprite.Sprite(
+            self.image,
+            x=x_pos,
+            y=Player.image.height)
+        self.sprite.scale = Bullet.scale
+        self.destroyed = False
+
+    def update(self, delta_time):
+        """Move the bullet up"""
+        self.sprite.y += self.speed * delta_time
+
+    def draw(self):
+        """Draw the bullet sprite"""
+        self.sprite.draw()
+
+
+class Laser(Bullet):
+    """Like bullets, but green and they travel downwards!"""
+    image = pyglet.resource.image("images/laser.png")
+    speed = -180
+
+    def __init__(self, x_pos, y_pos):
+        """Initialise a newly created bullet"""
+        super(Laser, self).__init__(x_pos)
+        self.sprite.y = y_pos - self.image.height
+
+
+class Player(object):
+    """Handles the details of the player's avatar."""
+
+    image = pyglet.resource.image("images/player.png")
+    speed = 150
+    cooldown_time = 0.5
+
+    left_key = pyglet.window.key.LEFT
+    right_key = pyglet.window.key.RIGHT
+    fire_key = pyglet.window.key.SPACE
+
+    def __init__(self, window):
+        """Create a new Player instance"""
+        self.sprite = pyglet.sprite.Sprite(Player.image, x=20, y=20)
+        self.key_handler = pyglet.window.key.KeyStateHandler()
+        self.window = window
+        self.cooldown = False
+
+    def draw(self):
+        """Draw the sprite"""
+        self.sprite.draw()
+
+    def update(self, delta_time):
+        """Move the sprite if appropriate.
+        Use the speed and the delta_time provided"""
+        if self.key_handler[Player.left_key] and \
+                not self.key_handler[Player.right_key]:
+            self.move(0-Player.speed, delta_time)
+        elif self.key_handler[Player.right_key] and \
+                not self.key_handler[Player.left_key]:
+            self.move(Player.speed, delta_time)
+
+    def move(self, speed, elapsed_time):
+        """Simply do speed * time to get a movement distance"""
+        self.sprite.x += speed * elapsed_time
+
+    def on_key_press(self, symbol, modifiers):
+        """Used for the less frequent key: firing!"""
+        if symbol == pyglet.window.key.SPACE and not self.cooldown:
+            self.fire()
+            self.cooldown = True
+            pyglet.clock.schedule_once(self.end_cooldown, Player.cooldown_time)
+
+    def end_cooldown(self, delta_time=None):
+        """Set cooldown to false, enabling you to fire again"""
+        self.cooldown = False
+
+    def fire(self):
+        """Fire a bullet!"""
+        self.window.bullets.append(
+            Bullet(self.sprite.x + self.sprite.width / 2))
+
+    def explode(self):
+        """Turn into a nice explosion!"""
+        self.sprite = pyglet.sprite.Sprite(
+            Alien.explosion_image,
+            x=self.sprite.x,
+            y=self.sprite.y)
+        self.sprite.draw()
+
+
+class Alien(object):
+    """Handles all of the joys of being an alien"""
+
+    image = pyglet.resource.image("images/invader.png")
+    explosion_image = pyglet.resource.image("images/explosion.png")
+    strafe_step = 50
+    strafe_delay = 1
+    lurch_delay = 5
+    victory_threshold = 50
+    likelihood_to_fire = 0.001
+
+    def __init__(self, window, x_pos):
+        """Set up a new alien"""
+        self.sprite = pyglet.sprite.Sprite(
+            Alien.image,
+            x=x_pos,
+            y=window.height - Alien.image.height)
+
+        self.head_right = True
+
+        pyglet.clock.schedule_interval(self.strafe, Alien.strafe_delay)
+
+        self.exploded = False
+        self.destroyed = False
+
+        self.window = window
+
+    def strafe(self, delta_time):
+        """Do that characteristic lurch across the screen"""
+        if self.head_right:
+            self.sprite.x += Alien.strafe_step * delta_time
+        else:
+            self.sprite.x -= Alien.strafe_step * delta_time
+
+    def draw(self):
+        """Draw the sprite"""
+        self.sprite.draw()
+
+    def lurch(self):
+        """Lurch forward, towards the player.
+        Returns false if the alien has reached the victory threshold"""
+        self.sprite.y -= self.sprite.height + 5
+        return self.sprite.y > Alien.victory_threshold
+
+    def explode(self):
+        """Turn into a nice explosion!"""
+        self.exploded = True
+        self.sprite = pyglet.sprite.Sprite(
+            Alien.explosion_image,
+            x=self.sprite.x,
+            y=self.sprite.y)
+        pyglet.clock.schedule_once(self.destroy, 0.2)
+
+    def destroy(self, delta_time):
+        """Make this alien destroyed so it gets removed"""
+        self.destroyed = True
+
+    def fire(self):
+        """Fire the LASERS! Maybe."""
+        if random() < Alien.likelihood_to_fire:
+            self.window.lasers.append(
+                Laser(
+                    self.sprite.x + self.sprite.width / 2,
+                    self.sprite.y))
