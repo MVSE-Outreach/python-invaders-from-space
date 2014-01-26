78d3acb64e8558780db9f78e145ac901e2702cbc
Drummond Ogilvie <spare@daogilvie.com>
2014-01-26 15:19:52 +0000

    Shaving a few lines off, adding some comments. Making the update function independent of game over state.

--- invaders.py
+++ invaders.py
@@ -1,6 +1,8 @@
 """Invaders From Space! A Totally Original Game For Digimakers"""
+from itertools import chain
 
 import pyglet
+
 from objects import Player, Alien
 
 
@@ -13,42 +15,36 @@ class InvadersWindow(pyglet.window.Window):
         # Create pyglet window - the caption is the window title
         super(InvadersWindow, self).__init__(caption="Invaders From Space!")
 
-        # Game over label, also used for checking the state
+        # Game over label. We also use it as a flag for when
+        # the game is finished.
         self.game_over_label = None
 
-        #  Add the alien tracker
+        #  Add the alien and laser lists
         self.aliens = []
-        self.spawn_alien_row(number_of_aliens=4)
-
         self.lasers = []
 
-        # Make it a bit harder by dumping a load of invaders
+        # Start the game with three loads of aliens
+        self.lurch_aliens_forward()
         self.lurch_aliens_forward()
         self.lurch_aliens_forward()
 
-        # Add the timed functions to make them jump about
+        # We add two timed functions here to control the flow of aliens
         pyglet.clock.schedule_interval(self.change_alien_direction, 5)
         pyglet.clock.schedule_interval(self.lurch_aliens_forward, 5)
 
         # Add the player and bullet tracker
         self.player = Player(window=self)
         self.push_handlers(self.player.key_handler)
-
         self.bullets = []
 
     def on_draw(self):
         """Main draw loop. Here is where things actually get
         written to the screen"""
-        super(InvadersWindow, self).clear()
+        self.clear()
         self.player.draw()
-        for bullet in self.bullets:
-            bullet.draw()
 
-        for alien in self.aliens:
-            alien.draw()
-
-        for laser in self.lasers:
-            laser.draw()
+        for drawable in chain(self.bullets, self.aliens, self.lasers):
+            drawable.draw()
 
         if self.game_over_label is not None:
             self.game_over_label.draw()
@@ -56,48 +52,47 @@ class InvadersWindow(pyglet.window.Window):
     def update(self, delta_time):
         """Perform frame-rate indepent updates of game objects"""
 
-        if self.game_over_label is None:
-            # have_collided = InvadersWindow.have_collided
-            self.player.update(delta_time=delta_time)
+        # have_collided = InvadersWindow.have_collided
+        self.player.update(delta_time=delta_time)
 
-            # Update all the bullets
-            for bullet in self.bullets:
-                bullet.update(delta_time=delta_time)
-                # Check collisions
-                for alien in self.aliens:
-                    if bullet.has_hit(alien):
-                        bullet.destroy()
-                        alien.explode()
-
-            # Update all the lasers
-            for laser in self.lasers:
-                laser.update(delta_time=delta_time)
-                # Check collision
-                if laser.has_hit(self.player):
-                    laser.destroy()
-                    self.player.explode()
-                    self.game_over(you_won=False)
-
-            # Remove bullets that have gone off the screen
-            self.bullets = [
-                b for b in self.bullets if b.sprite.y < self.height
-                and not b.destroyed]
-
-            # Remove the aliens that are destroyed
-            self.aliens = [a for a in self.aliens if not a.destroyed]
-
-            # Remove lasers that have gone off the screen
-            self.lasers = [
-                l for l in self.lasers if l.sprite.y > 0
-                and not l.destroyed]
-
-            # Make the aliens fire!
+        # Update all the bullets
+        for bullet in self.bullets:
+            bullet.update(delta_time=delta_time)
+            # Check collisions
             for alien in self.aliens:
-                alien.fire()
+                if bullet.has_hit(alien):
+                    bullet.destroy()
+                    alien.explode()
 
-            # Do the end game victory check
-            if len(self.aliens) == 0:
-                self.game_over(you_won=True)
+        # Update all the lasers
+        for laser in self.lasers:
+            laser.update(delta_time=delta_time)
+            # Check collisions
+            if laser.has_hit(self.player):
+                laser.destroy()
+                self.player.explode()
+                self.game_over(you_won=False)
+
+        # Remove bullets that have gone off the screen
+        self.bullets = [
+            b for b in self.bullets if b.sprite.y < self.height
+            and not b.destroyed]
+
+        # Remove the aliens that are destroyed
+        self.aliens = [a for a in self.aliens if not a.destroyed]
+
+        # Remove lasers that have gone off the screen
+        self.lasers = [
+            l for l in self.lasers if l.sprite.y > 0
+            and not l.destroyed]
+
+        # Make the aliens fire!
+        for alien in self.aliens:
+            alien.fire()
+
+        # Do the end game victory check
+        if len(self.aliens) == 0:
+            self.game_over(you_won=True)
 
     def change_alien_direction(self, delta_time):
         """Make aliens strafe in a different direction"""
